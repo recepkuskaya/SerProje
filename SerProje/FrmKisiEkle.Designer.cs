8688d769455b72212dd47fddabd69ab6a6dd013d
Recep Kuşkaya <recepgs3839@gmail.com>
2024-05-28 21:16:02 +0300

    Kişi ekleme ekranı eklendi

---
+++ SerProje/FrmKisiEkle.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace SerProje
+{
+    partial class FrmKisiEkle
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_KayitNo = new System.Windows.Forms.Label();
+            this.btn_Ekle = new System.Windows.Forms.Button();
+            this.btn_Guncelle = new System.Windows.Forms.Button();
+            this.btn_Sil = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmb_Parent = new System.Windows.Forms.ComboBox();
+            this.mskTxtTelefonNo = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "T.C. Kimlik No :";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.maskedTextBox1);
+            this.panel2.Controls.Add(this.mskTxtTelefonNo);
+            this.panel2.Controls.Add(this.cmb_Parent);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.btn_Sil);
+            this.panel2.Controls.Add(this.btn_Guncelle);
+            this.panel2.Controls.Add(this.btn_Ekle);
+            this.panel2.Controls.Add(this.lbl_KayitNo);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtSoyadi);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtAdi);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(575, 258);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Adı :";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdi.Location = new System.Drawing.Point(195, 47);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(245, 26);
+            this.txtAdi.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Soyadı :";
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoyadi.Location = new System.Drawing.Point(195, 79);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(245, 26);
+            this.txtSoyadi.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Telefon No :";
+            // 
+            // lbl_KayitNo
+            // 
+            this.lbl_KayitNo.AutoSize = true;
+            this.lbl_KayitNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_KayitNo.ForeColor = System.Drawing.Color.Red;
+            this.lbl_KayitNo.Location = new System.Drawing.Point(482, 21);
+            this.lbl_KayitNo.Name = "lbl_KayitNo";
+            this.lbl_KayitNo.Size = new System.Drawing.Size(75, 20);
+            this.lbl_KayitNo.TabIndex = 9;
+            this.lbl_KayitNo.Text = "Kayıt No";
+            this.lbl_KayitNo.Visible = false;
+            // 
+            // btn_Ekle
+            // 
+            this.btn_Ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ekle.ForeColor = System.Drawing.Color.ForestGreen;
+            this.btn_Ekle.Location = new System.Drawing.Point(66, 196);
+            this.btn_Ekle.Name = "btn_Ekle";
+            this.btn_Ekle.Size = new System.Drawing.Size(92, 32);
+            this.btn_Ekle.TabIndex = 10;
+            this.btn_Ekle.Text = "Ekle";
+            this.btn_Ekle.UseVisualStyleBackColor = true;
+            // 
+            // btn_Guncelle
+            // 
+            this.btn_Guncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Guncelle.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_Guncelle.Location = new System.Drawing.Point(195, 196);
+            this.btn_Guncelle.Name = "btn_Guncelle";
+            this.btn_Guncelle.Size = new System.Drawing.Size(107, 32);
+            this.btn_Guncelle.TabIndex = 11;
+            this.btn_Guncelle.Text = "Güncelle";
+            this.btn_Guncelle.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sil
+            // 
+            this.btn_Sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sil.ForeColor = System.Drawing.Color.Red;
+            this.btn_Sil.Location = new System.Drawing.Point(348, 196);
+            this.btn_Sil.Name = "btn_Sil";
+            this.btn_Sil.Size = new System.Drawing.Size(92, 32);
+            this.btn_Sil.TabIndex = 12;
+            this.btn_Sil.Text = "Sil";
+            this.btn_Sil.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Bağlı Olduğu Kişi :";
+            // 
+            // cmb_Parent
+            // 
+            this.cmb_Parent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Parent.FormattingEnabled = true;
+            this.cmb_Parent.Location = new System.Drawing.Point(195, 143);
+            this.cmb_Parent.Name = "cmb_Parent";
+            this.cmb_Parent.Size = new System.Drawing.Size(245, 28);
+            this.cmb_Parent.TabIndex = 14;
+            // 
+            // mskTxtTelefonNo
+            // 
+            this.mskTxtTelefonNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mskTxtTelefonNo.Location = new System.Drawing.Point(195, 111);
+            this.mskTxtTelefonNo.Mask = "(999) 000-0000";
+            this.mskTxtTelefonNo.Name = "mskTxtTelefonNo";
+            this.mskTxtTelefonNo.Size = new System.Drawing.Size(245, 26);
+            this.mskTxtTelefonNo.TabIndex = 15;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(195, 18);
+            this.maskedTextBox1.Mask = "00000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(245, 26);
+            this.maskedTextBox1.TabIndex = 16;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 350);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox mskTxtTelefonNo;
+        private System.Windows.Forms.ComboBox cmb_Parent;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Sil;
+        private System.Windows.Forms.Button btn_Guncelle;
+        private System.Windows.Forms.Button btn_Ekle;
+        private System.Windows.Forms.Label lbl_KayitNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdi;
+    }
+}
+
--- SerProje/FrmKisiEkle.cs
+++ SerProje/FrmKisiEkle.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SerProje
 {
-    public partial class Form1 : Form
+    public partial class FrmKisiEkle : Form
     {
-        public Form1()
+        public FrmKisiEkle()
         {
             InitializeComponent();
         }
