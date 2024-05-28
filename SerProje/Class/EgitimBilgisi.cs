5503b4f00d8230ba54fb3bf58ea8b5bef4c06017
recepkuskaya <37511691+recepkuskaya@users.noreply.github.com>
2024-05-28 22:56:41 +0300

    kişi ve eğitim bilgileri için class lar eklendi.

---
+++ SerProje/Class/EgitimBilgisi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerProje.Class
+{
+    public class EgitimBilgisi
+    {
+        private int KisiId_;
+        private string OkulTuru_;
+        private string OkulAdi_;
+        private string OkulIl_;
+        private string OkulIlce_;
+        private int KayitYil_;
+        private int MezuniyetYil_;
+
+        public int Kisi_Id
+        {
+            get { return KisiId_; }
+            set { KisiId_ = value; }
+        }
+
+        public string Okul_Turu
+        {
+            get { return OkulTuru_; }
+            set { OkulTuru_ = value; }
+        }
+
+        public string Okul_Adi
+        {
+            get { return OkulAdi_; }
+            set { OkulAdi_ = value; }
+        }
+
+        public string Okul_Il
+        {
+            get { return OkulIl_; }
+            set { OkulIl_ = value; }
+        }
+
+        public string Okul_Ilce
+        {
+            get { return OkulIlce_; }
+            set { OkulIlce_ = value; }
+        }
+
+        public int Kayit_Yil
+        { 
+            get { return KayitYil_; }
+            set { KayitYil_ = value; }
+        }
+
+        public int Mezuniyet_Yil
+        {
+            get { return MezuniyetYil_; }
+            set { MezuniyetYil_ = value; }
+        }
+    }
+}
---
+++ SerProje/Class/KisiBilgisi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerProje.Class
+{
+    public class KisiBilgisi
+    {
+        private string TcKimlikNo_;
+        private string Adi_;
+        private string Soyadi_;
+        private string TelefonNo_;
+        private int BagliKisiNo_;
+        private byte IsParent_;
+
+
+        public string TcKimlikNo
+        {
+            get { return TcKimlikNo_; }
+            set { TcKimlikNo_= value; }
+        }
+
+        public string Adi
+        {
+            get { return Adi_; }
+            set { Adi_ = value; }
+        }
+
+        public string Soyadi
+        {
+            get { return Soyadi_; }
+            set { Soyadi_ = value; }
+        }
+
+        public string TelefonNo
+        {
+            get { return TelefonNo_; }
+            set { TelefonNo_ = value; }
+        }
+
+        public int BagliOlduguKisiNo
+        {
+            get { return BagliKisiNo_; }
+            set { BagliKisiNo_ = value; }
+        }
+
+        public byte IsParent
+        {
+            get { return IsParent_; }
+            set { IsParent_ = value; }
+        }
+        
+    }
+}
